a5a9558dab12d6d6e40377909ec3619e007e9894
sharif37 <sharif.csecu@gmail.com>
2024-07-31 23:07:40 +0600

    added all routes of product table

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -88,4 +88,206 @@ productRouter.get("/allProduct", verifySession, async (req, res) => {
   }
 });
 
+productRouter.get("/:id", async (req, res) => {
+  try {
+    const product_id = z.coerce.string().parse(req.params.id);
+    const data = await db
+      .selectFrom("Product")
+      .selectAll()
+      .where("Product.product_id", "=", product_id)
+      .executeTakeFirst();
+
+    if (!data) {
+      return res
+        .status(404)
+        .json({ message: "Product with id " + product_id + " not found" });
+    }
+
+    return res.status(200).json({ ...data });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({
+        name: "Invalid data type.",
+        message: JSON.parse(error.message),
+      });
+    }
+
+    return res.status(500).json({ message: "Internal server error", error });
+  }
+});
+
+const productUpdateBody = z.object({
+  seller_id: z.string().optional(),
+  product_name: z.string().optional(),
+  category: z.string().optional(),
+  price: z.number().optional(),
+  quantity: z.number().optional(),
+  is_imported: z.boolean().optional(),
+  sub_category: z.string().optional(),
+  old_price: z.number().optional(),
+  description: z.any().optional(),
+  rating: z.number().min(0).max(5).optional(),
+  quality_type: z.string().optional(),
+  terms_and_conditions: z.any().optional(),
+  highlights: z.string().optional(),
+  isPopular: z.boolean().optional(),
+  fav_count: z.number().optional(),
+  is_out_of_stock: z.boolean().optional(),
+});
+
+productRouter.put(
+  "/:id",
+  verifySession,
+  requireRole([Role.Seller, Role.Admin]),
+  async (req, res) => {
+    try {
+      const product_id = z.coerce.string().parse(req.params.id);
+      const productInfo = productUpdateBody.parse(req.body);
+      const hasUpdate = Object.keys(productInfo).length > 0;
+
+      if (!hasUpdate) {
+        return res.status(400).json({
+          name: "Invalid update.",
+          message: "Nothing to update.",
+        });
+      }
+
+      let query = db
+        .updateTable("Product")
+        .where("Product.product_id", "=", product_id);
+
+      if (productInfo.seller_id) {
+        query = query.set({
+          seller_id: productInfo.seller_id,
+        });
+      }
+
+      if (productInfo.product_name) {
+        query = query.set({ product_name: productInfo.product_name });
+      }
+
+      if (productInfo.category) {
+        query = query.set({ category: productInfo.category });
+      }
+
+      if (productInfo.price) {
+        query = query.set({ price: productInfo.price });
+      }
+
+      if (productInfo.quantity) {
+        query = query.set({ quantity: productInfo.quantity });
+      }
+
+      if (productInfo.is_imported !== undefined) {
+        query = query.set({ is_imported: productInfo.is_imported ? 1 : 0 });
+      }
+
+      if (productInfo.sub_category) {
+        query = query.set({ sub_category: productInfo.sub_category });
+      }
+
+      if (productInfo.old_price) {
+        query = query.set({ old_price: productInfo.old_price });
+      }
+
+      if (productInfo.description) {
+        query = query.set({
+          description: JSON.stringify(productInfo.description),
+        });
+      }
+
+      if (productInfo.rating) {
+        query = query.set({ rating: productInfo.rating });
+      }
+
+      if (productInfo.quality_type) {
+        query = query.set({ quality_type: productInfo.quality_type });
+      }
+
+      if (productInfo.terms_and_conditions) {
+        query = query.set({
+          terms_and_conditions: JSON.stringify(
+            productInfo.terms_and_conditions,
+          ),
+        });
+      }
+
+      if (productInfo.highlights) {
+        query = query.set({ highlights: productInfo.highlights });
+      }
+
+      if (productInfo.isPopular !== undefined) {
+        query = query.set({ isPopular: productInfo.isPopular ? 1 : 0 });
+      }
+
+      if (productInfo.fav_count) {
+        query = query.set({ fav_count: productInfo.fav_count });
+      }
+
+      if (productInfo.is_out_of_stock !== undefined) {
+        query = query.set({
+          is_out_of_stock: productInfo.is_out_of_stock ? 1 : 0,
+        });
+      }
+
+      await query.execute();
+
+      return res.status(200).json({ message: "Product updated successfully." });
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({
+          name: "Invalid data type.",
+          message: JSON.parse(error.message),
+        });
+      }
+      console.error("Error updating product:", error);
+      return res.status(500).json({ message: "Internal server error", error });
+    }
+  },
+);
+
+const productIdSchema = z.object({
+  id: z.coerce.string(),
+});
+
+productRouter.delete(
+  "/delete/:id",
+  verifySession,
+  requireRole([Role.Admin, Role.Seller]),
+  async (req, res) => {
+    try {
+      const { id } = productIdSchema.parse(req.params);
+
+      const query = await db
+        .deleteFrom("Product")
+        .where("Product.product_id", "=", id)
+        .executeTakeFirst();
+        console.log(query) ;
+
+      if (query.numDeletedRows=== BigInt(0)) {
+        return res.status(404).json({
+          message: `Product with ID ${id} not found`,
+        });
+      }
+
+      return res.status(200).json({
+        message: "Product deleted successfully",
+      });
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return res.status(400).json({
+          name: "Invalid data type",
+          message: error.errors,
+        });
+      }
+
+      console.error("Error deleting product:", error);
+      return res.status(500).json({
+        message: "Internal server error",
+        error: error,
+      });
+    }
+  }
+);
+
 export default productRouter;
